b52bf09433915f082c05f810aeaeff0885257977
Jeremy Brudvik <jeremy@brudvik.me>
2022-05-13 11:05:28 -0400

    Bump version: v0.0.6

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		ArgsUsage:       "<text to append>",
 		Description:     "Ignores shared notes. Formats as new line by default.",
 		HideHelpCommand: true,
-		Version:         "v0.0.5",
+		Version:         "v0.0.6",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:    "bulleted",
